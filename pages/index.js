23ba97bd9706fe0e7470aabe3c8730b3e1f4cbd3
user <user@usernoMacBook-Pro.local>
2022-06-02 17:06:53 +0545

    Initalize firebase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import 'antd/dist/antd.css';
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-
+        <h1>Bipan</h1>
       </main>
     </div>
   )
